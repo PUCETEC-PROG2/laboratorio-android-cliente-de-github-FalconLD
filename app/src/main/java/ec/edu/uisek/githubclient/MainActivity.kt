02f5bbbb20fe38a4e88a90b6094fdfd76a325bc5
FalconLD <leodavid_a@hotmail.com>
2026-06-04 16:18:27 -0500

    Pantalla principal como contenedor

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,20 +1,72 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    RepoListFragment.RepoListListener,
+    RepoFormFragment.RepoFormListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            showListFragment()
+        }
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStackImmediate()
+                    refreshListIfVisible()
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+    }
+
+    override fun onAddRepositoryClick() {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, RepoFormFragment.newInstance())
+            addToBackStack(null)
         }
     }
+
+    override fun onEditRepositoryClick(repo: Repo) {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, RepoFormFragment.newInstance(repo))
+            addToBackStack(null)
+        }
+    }
+
+    override fun onFormCancelled() {
+        supportFragmentManager.popBackStackImmediate()
+    }
+
+    override fun onFormSaved() {
+        supportFragmentManager.popBackStackImmediate()
+        refreshListIfVisible()
+    }
+
+    private fun refreshListIfVisible() {
+        (supportFragmentManager.findFragmentById(R.id.fragment_container) as? RepoListFragment)
+            ?.fetchRepositories()
+    }
+
+    private fun showListFragment() {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, RepoListFragment.newInstance())
+        }
+    }
+
 }
